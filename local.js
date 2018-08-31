fb3e67720cce51826d3f42b08083e978772190cd
Romuald Quantin <romu@soundstep.com>
2018-08-31 14:16:50 +0100

    Cleanup.

--- local.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const simplePactJson = require('./tests/fixtures/simple-pact.json');
-
-const server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(simplePactJson));
-});
-server.listen(9012);
